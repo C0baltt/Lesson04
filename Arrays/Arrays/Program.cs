c59cdbfafcf5d5ec74a825ea20b4fbfc1b03d2b2
Александр Жаворонок <C0baltt@live.com>
2021-04-10 10:47:54 +0300

    develop 1st program array is finish

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -21,19 +21,55 @@ namespace Arrays
                     }
                     else
                     {
-                        Console.WriteLine("Input only numbers from 3 to 10:");
-                        continue;
+                        Console.WriteLine("Input only numbers from 3 to 10!\n");
                     }
                 }
             }
 
             var numberOfArrayElements = ReadNumberOfArrayElements();
+            int[] ArrayElements = new int[numberOfArrayElements];
 
             void ReadArrayElements()
             {
-
+                for(int i = 0; i < numberOfArrayElements;)
+                {
+                    Console.WriteLine($"Input an element № {i}");
+                    var input = Console.ReadLine();
+                    if (Int32.TryParse(input, out int elementOfArray))
+                    {
+                        ArrayElements[i] = elementOfArray;
+                        i++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input only numbers!\n");
+                    }
+                }
             }
 
+            ReadArrayElements();
+
+            void WriteArrayOfElements()
+            {
+                foreach (var i in ArrayElements)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Array of elements:");
+            WriteArrayOfElements();
+
+            void WriteReverseArray()
+            {
+                for (int i = ArrayElements.Length - 1; i >= 0; i--)
+                {
+                    Console.Write($"{ArrayElements[i]} ");
+                }
+            }
+            Console.WriteLine("Reverse array of elements:");
+            WriteReverseArray(); 
         }
     }
 }
