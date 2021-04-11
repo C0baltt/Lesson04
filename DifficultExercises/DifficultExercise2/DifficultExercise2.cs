8627e085e30feb054af2966516a1966b6a42ff0d
Александр Жаворонок <C0baltt@live.com>
2021-04-11 20:05:19 +0300

    Development 2nd program from difficult exercises is finish.

---
+++ DifficultExercises/DifficultExercise2/DifficultExercise2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DifficultExercise2
+{/*
+  Given an array nums of integers, return how many of item contain an even number of digits
+  */
+    class DifficultExercise2
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new[] { 12, 345, 2, 6, 7896 };
+
+            
+            printArray();
+            void printArray()
+            {
+                foreach (var item in nums)
+                {
+                    Console.Write($"{item} ");
+                }
+                Console.Write("\n");
+            }
+
+            int evenNumbers = 0;
+
+            foreach (var item in nums)
+            {
+                var temp = item;
+                var numberOfDigits = 0;
+
+                while ((temp % 10) != 0)
+                {
+                    temp /= 10;
+                    numberOfDigits++;
+                }
+
+                if ((numberOfDigits % 2) == 0)
+                {
+                    evenNumbers++;
+                }
+            }
+
+            Console.WriteLine($"Even numbers = {evenNumbers}");
+        }
+    }
+}
