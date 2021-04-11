5dd89c8b3d967d2048f3abb30e17b956ef5f4095
Александр Жаворонок <C0baltt@live.com>
2021-04-11 14:15:48 +0300

    Development 3st program string is finish.

---
+++ Strings/String3/String3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace String3
+{
+    class String3
+    {
+        static void Main(string[] args)
+        {
+            string ReadString(int stringNumber)
+            {
+                Console.WriteLine($"Input string № {stringNumber}:");
+                string str = Console.ReadLine();
+                return str;
+            }
+            string str1 = ReadString(1);
+            string str2 = ReadString(2);
+            string str3 = ReadString(3);
+
+            Console.WriteLine("\nPrint concotenate strings:"); 
+            Console.WriteLine($"{String.Concat(str1, str2, str3)}\n");
+        }
+    }
+}
