839b8834ec0295d7eeae113349033684174eaa54
Александр Жаворонок <C0baltt@live.com>
2021-04-11 18:58:17 +0300

    Development 1th program from difficult exercises is finish.

---
+++ DifficultExercises/DifficultExercises/DifficultExercise1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DifficultExercises
+{
+    /*
+     Given an integer array nums. Move all 0 to the end of while maintaining the relative order of the non-zero elements
+    that you must do this in-place without making a copy of the array
+     */
+    class DifficultExercise1
+    {
+        static void Main(string[] args)
+        {
+            int[] nums  = new[] { 0, 1, 0, 3, 12 };
+
+            int numberOfZero = 0;
+
+            printArray();
+            void printArray()
+            {
+                foreach (var item in nums)
+                {
+                    Console.Write($"{item} ");
+                }
+                Console.Write("\n");
+            }
+
+            foreach (var item in nums)
+            {
+                if (item == 0)
+                {
+                    numberOfZero++;
+                }
+            }
+
+            while (numberOfZero != 0)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == 0)
+                    {
+                        if (i + 1 != nums.Length)
+                        {
+                            int temp = 0;
+                            nums[i] = nums[i + 1];
+                            nums[i + 1] = temp;
+                        }
+                    }
+                }
+                numberOfZero--;
+            }
+
+            printArray();
+        }
+    }
+}
