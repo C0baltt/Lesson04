e07eaeed09eb230a9293a9c8da127c0c76c329b3
Александр Жаворонок <C0baltt@live.com>
2021-04-11 14:44:33 +0300

    Development 4th program string is finish.

---
+++ Strings/String4/String4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace String4
+{/*
+  41. Write a C# Sharp program to indicate whether each string in an array ends with a period (".").
+  */
+    class String4
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nInput string or type \"q\" for quit");
+                string str = Console.ReadLine();
+                
+                if (str == "q")
+                {
+                    return;
+                }
+                
+                Console.Write($"Does string ends in a \".\"? {str.EndsWith(".")}\n");
+            }
+        }
+    }
+}
