d307b06fe1d9200bb5e3848ae54f89b04cb52f3d
Александр Жаворонок <C0baltt@live.com>
2021-04-11 17:32:35 +0300

    Development 5th program string is finish.

---
+++ Strings/String5/String5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace String5
+{/*
+  15. Write a program in C# Sharp to read a sentence and replace lowercase characters by uppercase and vice-versa. 
+  */
+    class String5
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nInput string or type \"q\" for quit");
+                string str = Console.ReadLine();
+
+                if (str == "q")
+                {
+                    return;
+                }
+
+                foreach (var item in str)
+                {
+                    var ch = item;
+                    if (Char.IsLower(ch))
+                    {
+                        ch = Char.ToUpper(ch);
+                        Console.Write(ch);
+                    }
+                    else
+                    {
+                        ch = Char.ToLower(ch);
+                        Console.Write(ch);
+                    }
+                }
+            }
+        }
+    }
+}
